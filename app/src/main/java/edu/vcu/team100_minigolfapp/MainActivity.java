bddecef573162733d1fcaed4e27f90986bf2f8ea
gullerpj <gullerpj@mymail.vcu.edu>
2021-03-05 12:20:52 -0500

    linked scorecard to new game button

--- app/src/main/java/edu/vcu/team100_minigolfapp/MainActivity.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/MainActivity.java
@@ -10,11 +10,25 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 private ImageButton settings_Button;
+private Button new_game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       new_game = findViewById(R.id.newGameButton);
+        new_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScore();
+            }
+        });
         settings_Button =(ImageButton) findViewById(R.id.settings);
+
+
+
+
+
+
         settings_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +40,8 @@ public void openSettings(){
    Intent intent = new Intent(this,Settings.class);
     startActivity(intent);
 }
-
+    public void openScore(){
+        Intent intent = new Intent(this,ScoreCard.class);
+        startActivity(intent);
+    }
 }
