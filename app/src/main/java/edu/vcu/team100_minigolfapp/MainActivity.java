b64d2dbf3893c6db53a380de1cf0870aaa40ec6f
pedramvcu <malekip@vcu.edu>
2021-03-05 12:11:08 -0500

    finished the first page and the player page

--- app/src/main/java/edu/vcu/team100_minigolfapp/MainActivity.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/MainActivity.java
@@ -2,13 +2,35 @@ package edu.vcu.team100_minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button newGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        newGameButton = findViewById(R.id.newGameButton);
+
+
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(view.getContext() , ScoreCard.class));
+            }
+        });
+
+
+
+
+
     }
 }
