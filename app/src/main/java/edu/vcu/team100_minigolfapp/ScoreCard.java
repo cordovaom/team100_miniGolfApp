b96fdc62af89b054a1040f9f0556f48e197bfc7f
Austin Underwood <agugold3@gmail.com>
2021-03-05 01:54:15 -0500

    Hiding keyboard on unfocus

--- app/src/main/java/edu/vcu/team100_minigolfapp/ScoreCard.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/ScoreCard.java
@@ -2,11 +2,16 @@ package edu.vcu.team100_minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+
 import com.google.android.material.textfield.TextInputEditText;
 import java.util.ArrayList;
 
@@ -16,6 +21,7 @@ public class ScoreCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_card);
+
         Button done = findViewById(R.id.scorecardDone);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,5 +74,22 @@ public class ScoreCard extends AppCompatActivity {
             }
         }
         return views;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            View v = getCurrentFocus();
+            if ( v instanceof TextInputEditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 }
