2e3485870530c06cb74a0ac771d5ce70a0fa4f5c
gullerpj <gullerpj@mymail.vcu.edu>
2021-03-04 21:55:50 -0500

    Linked settings button on the home page to setting activity

--- app/src/main/java/edu/vcu/team100_minigolfapp/MainActivity.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/MainActivity.java
@@ -2,13 +2,29 @@ package edu.vcu.team100_minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+private ImageButton settings_Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings_Button =(ImageButton) findViewById(R.id.settings);
+        settings_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });
     }
+public void openSettings (){
+    Intent intent = new Intent(this,Settings.class);
+    startActivity(intent);
+}
+
 }
