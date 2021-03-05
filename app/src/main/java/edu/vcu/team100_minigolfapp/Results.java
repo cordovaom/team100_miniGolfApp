9bc11fd14a54d873ce9308193fe2ffa5213d6f98
Austin Underwood <agugold3@gmail.com>
2021-03-04 21:11:10 -0500

    Initial ScoreCard and Results screen setup

--- app/src/main/java/edu/vcu/team100_minigolfapp/Results.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/Results.java
@@ -2,7 +2,10 @@ package edu.vcu.team100_minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Results extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Button home = findViewById(R.id.resultsHome);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/vcu/team100_minigolfapp/ScoreCard.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/ScoreCard.java
@@ -2,7 +2,10 @@ package edu.vcu.team100_minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScoreCard extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ScoreCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_card);
+        Button done = findViewById(R.id.scorecardDone);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResultsActivity();
+            }
+        });
+    }
+
+    public void openResultsActivity() {
+        Intent intent = new Intent(this, Results.class);
+        startActivity(intent);
     }
 }
