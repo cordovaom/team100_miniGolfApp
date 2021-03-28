48cb533698cae03592851be67ef491bb9b8b6b96
Austin Underwood <agugold3@gmail.com>
2021-03-28 17:23:42 -0400

    Adding score inputs and calculations for players 3 and 4

--- app/src/main/java/edu/vcu/team100_minigolfapp/ScoreCard.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/ScoreCard.java
@@ -23,6 +23,8 @@ public class ScoreCard extends AppCompatActivity {
                 ViewGroup scorecard = findViewById(R.id.scrollView2);
                 int player1Total = 0;
                 int player2Total = 0;
+                int player3Total = 0;
+                int player4Total = 0;
 
                 ArrayList<View> player1HoleViews = getViewsByTag(scorecard, "player1");
                 for(int i = 0; i < player1HoleViews.size(); i++) {
@@ -42,9 +44,34 @@ public class ScoreCard extends AppCompatActivity {
                     player2Total += player2HoleScore;
                 }
 
+                ArrayList<View> player3HoleViews = getViewsByTag(scorecard, "player3");
+                for(int i = 0; i < player3HoleViews.size(); i++) {
+                    View view = player3HoleViews.get(i);
+                    TextInputEditText player3HoleEditText = findViewById(view.getId());
+                    String player3HoleText = player3HoleEditText.getText().toString();
+                    int player3HoleScore = (player3HoleText.isEmpty()) ? 0:Integer.parseInt(player3HoleText);
+                    player3Total += player3HoleScore;
+                }
+
+                ArrayList<View> player4HoleViews = getViewsByTag(scorecard, "player4");
+                for(int i = 0; i < player4HoleViews.size(); i++) {
+                    View view = player4HoleViews.get(i);
+                    TextInputEditText player4HoleEditText = findViewById(view.getId());
+                    String player4HoleText = player4HoleEditText.getText().toString();
+                    int player4HoleScore = (player4HoleText.isEmpty()) ? 0:Integer.parseInt(player4HoleText);
+                    player4Total += player4HoleScore;
+                }
+
+                /* Print statements for debugging purposes
                 System.out.println("Player 1 Total: " + player1Total);
                 System.out.println("Player 2 Total: " + player2Total);
-                String winner = ((player1Total<player2Total) ? "Player One" : (player2Total<player1Total) ? "Player Two":"Tie");
+                System.out.println("Player 3 Total: " + player3Total);
+                System.out.println("Player 4 Total: " + player4Total);
+                 */
+
+                int min = Math.min(Math.min(player1Total, player2Total), Math.min(player3Total, player4Total));
+
+                String winner = ((min==player1Total) ? "Player One" : (min==player2Total) ? "Player Two" : (min==player3Total) ? "Player Three" : (min==player4Total) ? "Player Four":"");
 
                 Intent intent = new Intent(v.getContext(), Results.class);
                 intent.putExtra("winner", winner);
@@ -52,6 +79,10 @@ public class ScoreCard extends AppCompatActivity {
                 intent.putExtra("P1Score", String.valueOf(player1Total));
                 intent.putExtra("P2Name", "Player Two");
                 intent.putExtra("P2Score", String.valueOf(player2Total));
+                intent.putExtra("P3Name", "Player Three");
+                intent.putExtra("P3Score", String.valueOf(player3Total));
+                intent.putExtra("P4Name", "Player Four");
+                intent.putExtra("P4Score", String.valueOf(player4Total));
                 startActivity(intent);
             }
         });
