03eb74244fbb99b559added48f1cb4c33b4ed9d3
cordovaom <43318741+cordovaom@users.noreply.github.com>
2021-04-28 14:13:19 -0400

    Changed results page to only show results for correct number of players.

--- app/src/main/java/edu/vcu/team100_minigolfapp/Results.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/Results.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Collections;
+
 public class Results extends AppCompatActivity {
 
     @Override
@@ -27,33 +29,47 @@ public class Results extends AppCompatActivity {
 
         if(!winner.equals("Tie")){
             ((TextView)findViewById(R.id.WinnerName)).setText(String.format("%s Wins", winner));
-            ((TextView)findViewById(R.id.P1Name)).setText(P1Name);
-            ((TextView)findViewById(R.id.P2Name)).setText(P2Name);
-            ((TextView)findViewById(R.id.P3Name)).setText(P3Name);
-            ((TextView)findViewById(R.id.P4Name)).setText(P4Name);
-            ((TextView)findViewById(R.id.P1Score)).setText(P1Score);
-            ((TextView)findViewById(R.id.P2Score)).setText(P2Score);
-            ((TextView)findViewById(R.id.P3Score)).setText(P3Score);
-            ((TextView)findViewById(R.id.P4Score)).setText(P4Score);
             ((View)findViewById(R.id.winnerIcon)).setVisibility(View.VISIBLE);
         } else {
-            ((TextView)findViewById(R.id.WinnerName)).setText(getString(R.string.tie));
-            ((TextView)findViewById(R.id.P1Name)).setText(P1Name);
-            ((TextView)findViewById(R.id.P2Name)).setText(P2Name);
-            ((TextView)findViewById(R.id.P3Name)).setText(P3Name);
-            ((TextView)findViewById(R.id.P4Name)).setText(P4Name);
-            ((TextView)findViewById(R.id.P1Score)).setText(P1Score);
-            ((TextView)findViewById(R.id.P2Score)).setText(P2Score);
-            ((TextView)findViewById(R.id.P3Score)).setText(P3Score);
-            ((TextView)findViewById(R.id.P4Score)).setText(P4Score);
+            ((TextView) findViewById(R.id.WinnerName)).setText(getString(R.string.tie));
             ((View)findViewById(R.id.winnerIcon)).setVisibility(View.INVISIBLE);
         }
+
+        if(Integer.parseInt(P1Score)!=0 || P1Name.length()!=0) {
+            ((TextView) findViewById(R.id.P1Name)).setText(P1Name);
+            ((TextView) findViewById(R.id.P1Score)).setText(P1Score);
+        }else{
+            ((View) findViewById(R.id.P1Name)).setVisibility(View.INVISIBLE);
+            ((View) findViewById(R.id.P1Score)).setVisibility(View.INVISIBLE);
+        }
+        if(Integer.parseInt(P2Score)!=0 || P2Name.length()!=0) {
+            ((TextView) findViewById(R.id.P2Name)).setText(P2Name);
+            ((TextView)findViewById(R.id.P2Score)).setText(P2Score);
+        }else{
+            ((View) findViewById(R.id.P2Name)).setVisibility(View.INVISIBLE);
+            ((View) findViewById(R.id.P2Score)).setVisibility(View.INVISIBLE);
+        }
+        if(Integer.parseInt(P3Score)!=0 || P3Name.length()!=0) {
+            ((TextView) findViewById(R.id.P3Name)).setText(P3Name);
+            ((TextView)findViewById(R.id.P3Score)).setText(P3Score);
+        }else{
+            ((View) findViewById(R.id.P3Name)).setVisibility(View.INVISIBLE);
+            ((View) findViewById(R.id.P3Score)).setVisibility(View.INVISIBLE);
+        }
+        if(Integer.parseInt(P4Score)!=0 || P4Name.length()!=0) {
+            ((TextView) findViewById(R.id.P4Name)).setText(P4Name);
+            ((TextView)findViewById(R.id.P4Score)).setText(P4Score);
+        }else{
+            ((View) findViewById(R.id.P4Name)).setVisibility(View.INVISIBLE);
+            ((View) findViewById(R.id.P4Score)).setVisibility(View.INVISIBLE);
+        }
+
 
         Button home = findViewById(R.id.resultsHome);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((TextView)findViewById(R.id.WinnerName)).setText("");
+               // ((TextView)findViewById(R.id.WinnerName)).setText("");
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 startActivity(intent);
             }
