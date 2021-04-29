ab880be97d282645a98da080ac0e6b14f114a82d
gullerpj <gullerpj@mymail.vcu.edu>
2021-04-29 13:23:51 -0400

    useles commit

--- app/src/main/java/edu/vcu/team100_minigolfapp/Settings.java
+++ app/src/main/java/edu/vcu/team100_minigolfapp/Settings.java
@@ -1,33 +1,30 @@
 package edu.vcu.team100_minigolfapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Settings extends AppCompatActivity {
-    private ImageButton settings_Button;
+    private Button color_button;
     private Button course_Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         //setting button to go back to settings may not be needed
-        //settings_Button =(ImageButton) findViewById(R.id.settings);
-        course_Button =(Button) findViewById(R.id.courses_button);
-
+        color_button =(Button) findViewById(R.id.color);
+        ConstraintLayout settings_view = findViewById(R.id.rlVar1);
 
         //listner for settings
-//        settings_Button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openSettings();
-//
-//            }
-//        });
+        color_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settings_view.setBackgroundColor(R.string.colorWhite);
+            }
+       });
     }
 //    public void openSettings(){
 //        Intent intent = new Intent(this,Settings.class);
